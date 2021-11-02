14c9bae36942a4853cff196750ab9b4e5e396151
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2021-11-02 11:57:25 +0400

    Fix

--- StringTrim/Program.cs
+++ StringTrim/Program.cs
@@ -8,41 +8,46 @@ namespace StringTrim
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter Input: ");
+            Console.Write("Enter Input:");
             string str = Convert.ToString(Console.ReadLine());
-             Console.WriteLine(Trim(str));
-            Console.WriteLine(Trim1(result));
+            Console.WriteLine($"Included Word Length:{str.Length}");
 
+            Console.WriteLine(Trim(str));
+            Console.WriteLine((Trim(str)).Length);
+          //  Console.WriteLine($"The length of the trimmed word:{Trim(str)).Length}");
+            
         }
         /// <summary>
-        /// Sozun evvelinden bosluqlarin silinmesi
+        /// Delete the space from the beginning of the word
         /// </summary>
-        /// <param name="str">istenilen sozu alir</param>
-        /// <returns>evveldn bosluqlari silinmis soz</returns>
-        static string Trim(string str)
-        {
-            bool check=false;
-            string result = string.Empty;
-          //  StringBuilder result = new StringBuilder();         
-            for (int i = 0; i <str.Length-1; i++)
-            {
-                if (str[i]!=' ')
-                {
-                    check = true;
-                   
-                }
-                if(check)
-                {
-                    result += str[i];
-                }
-            }
-            return result;
-        }
-        static string Trim1(string result)
+        /// <param name="str">Accepts a string type word or sentence.</param>
+        /// <returns>A word with spaces removed from the beginning of the word.</returns>
+        static string TrimBegin(string str)
         {
             bool check = false;
-            string result1 = string.Empty;
-            //  StringBuilder result = new StringBuilder();
+            StringBuilder begin = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != ' ')
+                {
+                    check = true;
+                }
+                if (check)
+                {
+                    begin.Append(str[i]);
+                }
+            }
+            return begin.ToString();
+        }
+        /// <summary>
+        /// Deletes spaces at the end of a word or sentence.
+        /// </summary>
+        /// <param name="result">A word returned in string type in the Trim1 method</param>
+        /// <returns>A word with spaces removed from the end of the word</returns>
+        static string TrimEnd(string result)
+        {
+            bool check = false;
+            StringBuilder end = new StringBuilder();
             for (int i = result.Length - 1; i >= 0; i--)
             {
                 if (result[i] != ' ')
@@ -51,10 +56,22 @@ namespace StringTrim
                 }
                 if (check)
                 {
-                    result1 += result[i];
+                    end.Append(result[i]);
                 }
             }
-            return result1;
+            //for (int i = 0; i < result.Length; i++)
+            //{
+            //    end.Append(result[i]);
+            //}
+            return end.ToString();
+        }
+        static string Trim(string str)
+        {
+            string result = string.Empty;
+            result = TrimBegin(str);
+            result = TrimEnd(result);
+            return result.ToString();
         }
     }
+
 }
